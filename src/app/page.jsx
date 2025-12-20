4a6bae7e2d5c8c5ea77156d733e9fe6b431fa823
Ossi05 <77546709+Ossi05@users.noreply.github.com>
2025-12-20 14:00:54 +0200

    Make HomePage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,31 @@
+import { ROUTES } from "@/config";
+import Image from "next/image";
+import Link from "next/link";
+import bgImage from "@/public/bg.png";
+
 export default async function HomePage() {
   return (
-    <div>
-      <h1>The Wild Oasis. Welcome to paradise!</h1>
+    <div className="mt-24">
+      <Image
+        src={bgImage}
+        fill
+        placeholder="blur"
+        quality={80}
+        className="object-cover object-top"
+        alt="Mountains and forests with two cabins"
+      />
+
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href={ROUTES.cabins}
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
     </div>
   );
 }
