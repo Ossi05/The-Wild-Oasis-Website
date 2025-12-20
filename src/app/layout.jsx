55447599a1618eeaff9763753e0dd18f8cfce1db
Ossi05 <77546709+Ossi05@users.noreply.github.com>
2025-12-20 12:54:13 +0200

    Add Josefin Sans font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,15 @@
 import "@/src/styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+
 import Logo from "@/src/components/Logo";
 import Navigation from "@/src/components/Navigation";
 import { WEBSITE_NAME } from "@/config";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+console.log(josefin);
 
 export const metadata = {
   title: {
@@ -15,7 +23,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
